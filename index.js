8a71185f9b033a9b569a06b4ffe4d419701660ff
Richard Willis <willis.rh@gmail.com>
2013-06-29 23:48:13 +0100

    Comments and whitespace fixes

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 var EventEmitter = require('events').EventEmitter;
 var sass = new EventEmitter();
 
+/**
+ * Prepare the command arguments
+ * @param  {object} options - The options passed to the compile method
+ * @return {array}
+ */
+
 function prepArgs(options) {
 
-	var args = [];
-	
-	if (options.filepath) {
-		args.push(options.filepath);
-	} else if (options.data) {
-		if (!options.type) {
-			options.type = 'scss';
-		}
-		args.push('--' + options.type);
-	} else {
-		throw new Error('Please either specify a filepath or data string to compile');
-	}
+  var args = [];
 
-	return args;
+  if (options.filepath) {
+    args.push(options.filepath);
+  } else if (options.data) {
+    if (!options.type) {
+      options.type = 'scss';
+    }
+    args.push('--' + options.type);
+  } else {
+    throw new Error('Please either specify a filepath or data string to compile');
+  }
+
+  return args;
 }
+
+/**
+ * Compiles the sass, either from a filepath or from a data string
+ * @param  {object} options - The compile options
+ */
 
 function compile(options) {
 
-	var args = prepArgs(options);
-	var child = require('child_process').spawn('sass', args);
+  var args = prepArgs(options);
+  var child = require('child_process').spawn('sass', args);
 
-	child.stdout.on('data', function (data) {
-		sass.emit('success', new Buffer(data).toString('utf8'));
-	});
+  child.stdout.on('data', function (data) {
+    sass.emit('success', new Buffer(data).toString('utf8'));
+  });
 
-	child.stderr.on('data', function (data) {
-		sass.emit('error', new Buffer(data).toString('utf8'));
-	});
+  child.stderr.on('data', function (data) {
+    sass.emit('error', new Buffer(data).toString('utf8'));
+  });
 
-	if (options.data) {
-		child.stdin.setEncoding('utf8');
-		child.stdin.write(options.data);
-    	child.stdin.end();
-    }
+  if (options.data) {
+    child.stdin.setEncoding('utf8');
+    child.stdin.write(options.data);
+    child.stdin.end();
+  }
 }
 
 sass.compile = compile;
